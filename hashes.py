1396a8ab8658c6fae7ed850c52b1519d752bc96a
zenshaf01 <zeeshanshafqat1@outlook.com>
2025-05-06 01:54:11 -0400

    added hashes with comments

---
+++ hashes.py
@@ -0,0 +1,15 @@
+'''
+Hash maps and hash sets are important for coding interviews.
+When you see the words 'unique', 'count', 'frequency' in a coding question, most probably a hash map or a hash set can be used to solve the problem. 
+
+Remember:
+Set: only contains keys and is unique
+Map: Contains key and value pairs and is unique
+
+Benefit of a hash map:
+- Constant time read, write
+
+Cons of hashmap:
+- Hash maps are not ordered. In order key traversal is not gauranteed.
+
+'''
