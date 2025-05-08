1034f6fe778e23b16a5ada9eca48fa369ee76d1e
zenshaf01 <zeeshanshafqat1@outlook.com>
2025-05-08 00:37:26 -0400

    updated

--- graphs.py
+++ graphs.py
@@ -5,15 +5,13 @@ A graph is an abstract concept and is also made up of nodes where the nodes migh
 Terminology:
 
 Graphs consist of edges and vertices
-Vertex: Each Vertex is also known as a node. Vertices are connected to eadch other using edges. A vertex can be connected to itself using an edge.
-Edge: Edges are pointers that connect the vertices. An edge can be unidirectional or bidirectional. The number of edges in a graqph always depends upon the number of vertices.
+Vertex: Each Vertex is also known as a node. Vertices are connected to each other using edges. A vertex can be connected to itself using an edge.
+Edge: Edges are pointers that connect the vertices. An edge can be unidirectional or bidirectional. The number of edges in a graph always depends upon the number of vertices.
 IF the edges have a direction, the graph is called a directed graph. Otherwise it is an undirected graph. Datastructures like trees, linked lists is a directed graph because the pointers are directed, prev, next etc.
-
-
-The number of edges in a graph will always be less than or equal to V^2. E <= V^2
+The number of edges in a graph will always be less than or equal to V^2. E <= V^2.
 
 Notes:
-It is possible that any node in a graph is nbot connected to eachother and it would still be considered a graph.
+It is possible that any node in a graph is not connected to eachother and it would still be considered a graph.
 A graph can be represented by many different concerete data structures.
 - Matrix
 - Adjacency Matrix
